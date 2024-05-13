7495994d686eb1bb23b4e7c9ce23535c82f49158
Adam Chance <6444703+drakon64@users.noreply.github.com>
2024-05-13 17:41:28 +0100

    Replace example Rust code

--- webhooks/queued/src/main.rs
+++ webhooks/queued/src/main.rs
@@ -1,19 +1,13 @@
-use lambda_http::{run, service_fn, tracing, Body, Error, Request, RequestExt, Response};
+use lambda_runtime::{run, service_fn, tracing, Error};
+use serde::{Deserialize, Serialize};
 
-async fn function_handler(event: Request) -> Result<Response<Body>, Error> {
-    let who = event
-        .query_string_parameters_ref()
-        .and_then(|params| params.first("name"))
-        .unwrap_or("world");
-    let message = format!("Hello {who}, this is an AWS Lambda HTTP request");
+#[derive(Deserialize)]
+struct Request {}
 
-    let resp = Response::builder()
-        .status(200)
-        .header("content-type", "text/html")
-        .body(message.into())
-        .map_err(Box::new)?;
-    Ok(resp)
-}
+#[derive(Serialize)]
+struct Response {}
+
+async fn function_handler(event: Request) -> Result<Response, Error> {}
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
