ac0cce9a236427f485beffb778b57c9979443cf6
Adam Chance <6444703+drakon64@users.noreply.github.com>
2024-05-15 23:59:55 +0100

    Serialise the GitHub webhook

--- webhooks/queued/src/main.rs
+++ webhooks/queued/src/main.rs
@@ -1,12 +1,29 @@
 use aws_lambda_events::apigw::ApiGatewayV2httpRequest;
 use lambda_runtime::{run, service_fn, tracing, Error, LambdaEvent};
+use serde::{Deserialize, Serialize};
 
-async fn function_handler(event: LambdaEvent<ApiGatewayV2httpRequest>) -> Result<String, Error> {
-    let body = event.payload.body.unwrap();
+#[derive(Deserialize, Serialize)]
+struct Webhook {
+    action: String,
+    workflow_job: WorkflowJob,
+    repository: Repository,
+}
 
-    println!("{}", body);
+#[derive(Deserialize, Serialize)]
+struct WorkflowJob {
+    run_id: u64,
+    run_attempt: usize,
+}
 
-    Ok(body)
+#[derive(Deserialize, Serialize)]
+struct Repository {
+    full_name: String,
+}
+
+async fn function_handler(event: LambdaEvent<ApiGatewayV2httpRequest>) -> Result<Webhook, Error> {
+    let webhook = serde_json::from_str::<Webhook>(&*event.payload.body.unwrap()).unwrap();
+
+    Ok(webhook)
 }
 
 #[tokio::main]
