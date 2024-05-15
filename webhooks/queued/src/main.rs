b250f3320ab11c58468f75606047ce22fd9913ed
Adam Chance <6444703+drakon64@users.noreply.github.com>
2024-05-15 23:35:30 +0100

    Fix webhook

--- webhooks/queued/src/main.rs
+++ webhooks/queued/src/main.rs
@@ -1,22 +1,12 @@
-use aws_lambda_events::apigw::{ApiGatewayProxyRequest, ApiGatewayProxyResponse};
-use aws_lambda_events::http::HeaderMap;
+use aws_lambda_events::apigw::ApiGatewayV2httpRequest;
 use lambda_runtime::{run, service_fn, tracing, Error, LambdaEvent};
 
-async fn function_handler(
-    event: LambdaEvent<ApiGatewayProxyRequest>,
-) -> Result<ApiGatewayProxyResponse, Error> {
+async fn function_handler(event: LambdaEvent<ApiGatewayV2httpRequest>) -> Result<String, Error> {
     let body = event.payload.body.unwrap();
-    let headers = HeaderMap::new();
 
     println!("{}", body);
 
-    Ok(ApiGatewayProxyResponse {
-        body: Some(body.into()),
-        headers: headers.clone(),
-        is_base64_encoded: false,
-        multi_value_headers: headers,
-        status_code: 200,
-    })
+    Ok(body)
 }
 
 #[tokio::main]
