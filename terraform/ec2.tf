ff84383d958edee7a15e0e4bc7e509aa3680e199
Adam Chance <6444703+drakon64@users.noreply.github.com>
2024-05-22 20:21:17 +0100

    Add the `CloudWatchAgentServerPolicy` managed policy to the EC2 IAM role

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -26,6 +26,8 @@ resource "aws_iam_role" "ec2" {
     name   = "GitHubActionsRunner"
     policy = data.aws_iam_policy_document.ec2.json
   }
+
+  managed_policy_arns = ["arn:aws:iam::aws:policy/CloudWatchAgentServerPolicy"]
 }
 
 resource "aws_iam_instance_profile" "ec2" {
