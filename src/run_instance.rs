6b4174ff8187c8ad87bfcdcaf97eb671b8da62fa
Adam Chance <6444703+drakon64@users.noreply.github.com>
2024-05-22 14:27:01 +0100

    Label runners with custom storage volume sizes

--- src/run_instance.rs
+++ src/run_instance.rs
@@ -13,43 +13,66 @@ use std::str::FromStr;
 pub(crate) async fn run_instance(client: Client, webhook: Webhook) -> Result<String, Error> {
     let repository_full_name = &webhook.repository.full_name;
 
+    let mut instance_type = InstanceType::M7gLarge;
+    let mut volume_size: i32 = 14; // This can fit in an u16
+
+    for label in &webhook.workflow_job.labels {
+        if label.starts_with("EC2-") {
+            instance_type = InstanceType::from_str(label.strip_prefix("EC2-").unwrap()).unwrap();
+        } else if label.starts_with("EBS-") {
+            volume_size = i32::from_str(
+                label
+                    .strip_prefix("EBS-")
+                    .unwrap()
+                    .strip_suffix("GB")
+                    .unwrap(),
+            )
+            .unwrap();
+        }
+    }
+
     // TODO: Get cloud-init to do this
     let user_data = BASE64_STANDARD.encode(format!("#!/bin/sh
+
+sysctl vm.swappiness=1
+mkswap /dev/nvme1n1
+swapon /dev/nvme1n1
 
 add-apt-repository ppa:ansible/ansible # https://github.com/ansible/ansible/issues/77624
 apt-get update
 apt-get -y install ansible-core awscli
 apt-get clean
 ansible-galaxy collection install amazon.aws community.general
-ansible-pull --url https://github.com/drakon64/github-actions-runner-aws.git --extra-vars 'url=https://github.com/{}' --extra-vars 'token={}' ansible/runner.yml"
-    , &repository_full_name, create_registration_token_for_repository(&repository_full_name, &webhook)));
-
-    let mut instance_type = InstanceType::M7gLarge;
-    let mut volume_size: i32 = 30; // This can fit in an u16
-
-    for label in webhook.workflow_job.labels {
-        if label.starts_with("EC2-") {
-            instance_type = InstanceType::from_str(label.strip_prefix("EC2-").unwrap()).unwrap();
-        } else if label.starts_with("EBS-") {
-            volume_size = i32::from_str(label.strip_prefix("EBS-").unwrap()).unwrap();
-        }
-    }
+ansible-pull --url https://github.com/drakon64/github-actions-runner-aws.git --extra-vars 'url=https://github.com/{}' --extra-vars 'token={}' --extra-vars='ebs_volume_size={}' ansible/runner.yml"
+    , &repository_full_name, create_registration_token_for_repository(&repository_full_name, &webhook), volume_size));
 
     let run_instances = client
         .run_instances()
         .instance_type(instance_type)
         .max_count(1)
         .min_count(1)
-        .set_block_device_mappings(Some(vec![BlockDeviceMapping::builder()
-            .set_device_name(Some("/dev/sda1".into()))
-            .set_ebs(Some(
-                EbsBlockDevice::builder()
-                    .set_delete_on_termination(Some(true))
-                    .set_volume_size(Some(volume_size))
-                    .set_volume_type(Some(VolumeType::Gp3))
-                    .build(),
-            ))
-            .build()]))
+        .set_block_device_mappings(Some(vec![
+            BlockDeviceMapping::builder()
+                .set_device_name(Some("/dev/sda1".into()))
+                .set_ebs(Some(
+                    EbsBlockDevice::builder()
+                        .set_delete_on_termination(Some(true))
+                        .set_volume_size(Some(volume_size))
+                        .set_volume_type(Some(VolumeType::Gp3))
+                        .build(),
+                ))
+                .build(),
+            BlockDeviceMapping::builder()
+                .set_device_name(Some("/dev/sdb".into()))
+                .set_ebs(Some(
+                    EbsBlockDevice::builder()
+                        .set_delete_on_termination(Some(true))
+                        .set_volume_size(Some(14))
+                        .set_volume_type(Some(VolumeType::Gp3))
+                        .build(),
+                ))
+                .build(),
+        ]))
         .set_launch_template(Some(
             LaunchTemplateSpecification::builder()
                 .set_launch_template_id(Some(env::var("ARM64_LAUNCH_TEMPLATE_ID").unwrap()))
