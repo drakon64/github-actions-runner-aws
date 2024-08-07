3750b63bc71d5152068df28a3f9f586776a077f0
Adam Chance <6444703+drakon64@users.noreply.github.com>
2024-08-07 18:46:57 +0100

    Install Grafana Alloy

--- src/run_instance.rs
+++ src/run_instance.rs
@@ -57,9 +57,13 @@ sysctl vm.swappiness=1
 mkswap /dev/nvme1n1
 swapon /dev/nvme1n1
 
+mkdir -p /etc/apt/keyrings/
+wget -q -O - https://apt.grafana.com/gpg.key | gpg --dearmor > /etc/apt/keyrings/grafana.gpg
+echo 'deb [signed-by=/etc/apt/keyrings/grafana.gpg] https://apt.grafana.com stable main' > /etc/apt/sources.list.d/grafana.list
+
 add-apt-repository ppa:ansible/ansible # https://github.com/ansible/ansible/issues/77624
 apt-get update
-apt-get -y install ansible-core awscli
+apt-get -y install ansible-core awscli alloy
 apt-get clean
 ansible-galaxy collection install amazon.aws community.general
 ansible-pull --url https://github.com/drakon64/github-actions-runner-aws.git --checkout canary --extra-vars 'url=https://github.com/{repository_full_name}' --extra-vars 'token={repository_registration_token}' --extra-vars '{{ \"spot\": {spot} }}' --extra-vars 'ebs_volume_size={volume_size}' ansible/runner.yml"));
