3ccade2578303c4b0efcf9f0ffa5eb0c10410cbe
Adam Chance <6444703+drakon64@users.noreply.github.com>
2024-08-07 22:17:44 +0100

    Temp: Log generated user data

--- src/user_data.rs
+++ src/user_data.rs
@@ -13,7 +13,7 @@ pub(crate) fn create_user_data(webhook: &Webhook, spot: bool, volume_size: &i32)
 
 aws ec2 create-tags --region {aws_region} --resources $(curl -H \"X-aws-ec2-metadata-token: $(curl -X PUT http://169.254.169.254/latest/api/token -H 'X-aws-ec2-metadata-token-ttl-seconds: 21600')\" http://169.254.169.254/latest/meta-data/instance-id/) --tags Key=Name,Value=\"${{GITHUB_REPOSITORY}}/${{GITHUB_RUN_ID}}/${{GITHUB_RUN_NUMBER}}/${{GITHUB_RUN_ATTEMPT}}/${{GITHUB_JOB}}\""));
 
-    BASE64_STANDARD.encode(format!("#!/bin/sh
+    let user_data = BASE64_STANDARD.encode(format!("#!/bin/sh
 
 sysctl vm.swappiness=1
 mkswap /dev/nvme1n1
@@ -38,5 +38,18 @@ apt-get update
 apt-get -y install ansible-core awscli alloy
 apt-get clean
 ansible-galaxy collection install community.general
-ansible-pull --url https://github.com/drakon64/github-actions-runner-aws.git --checkout canary --extra-vars 'url=https://github.com/{repository_full_name}' --extra-vars 'token={repository_registration_token}' --extra-vars '{{ \"spot\": {spot} }}' --extra-vars 'ebs_volume_size={volume_size}' ansible/runner.yml"))
+ansible-pull --url https://github.com/drakon64/github-actions-runner-aws.git --checkout canary --extra-vars 'url=https://github.com/{repository_full_name}' --extra-vars 'token={repository_registration_token}' --extra-vars '{{ \"spot\": {spot} }}' --extra-vars 'ebs_volume_size={volume_size}' ansible/runner.yml"));
+
+    // Temp
+    unsafe {
+        println!(
+            "{:?}",
+            String::from_utf8_unchecked(BASE64_STANDARD.decode(tag_script.clone()).unwrap())
+        );
+        println!(
+            "{:?}",
+            String::from_utf8_unchecked(BASE64_STANDARD.decode(user_data.clone()).unwrap())
+        );
+    }
+    user_data
 }
