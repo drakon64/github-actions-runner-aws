cd047f32fb4baa40534cf705e71e679a0fcfcd22
Adam Chance <6444703+drakon64@users.noreply.github.com>
2024-08-07 21:34:47 +0100

    Create `tag.sh` with user data

--- src/user_data.rs
+++ src/user_data.rs
@@ -2,6 +2,7 @@ use crate::github::create_registration_token_for_repository;
 use crate::webhook::Webhook;
 use base64::prelude::BASE64_STANDARD;
 use base64::Engine;
+use std::env;
 
 pub(crate) fn create_user_data(webhook: &Webhook, spot: bool, volume_size: &i32) -> String {
     let repository_full_name = &webhook.repository.full_name;
@@ -9,6 +10,18 @@ pub(crate) fn create_user_data(webhook: &Webhook, spot: bool, volume_size: &i32)
 
     let user_data_script = include_str!("files/user-data.sh");
 
+    let aws_region = env::var("AWS_REGION").unwrap();
+    let tag_script = BASE64_STANDARD.encode(format!("#!/bin/sh
+
+TOKEN=$(curl -X PUT http://169.254.169.254/latest/api/token -H 'X-aws-ec2-metadata-token-ttl-seconds: 21600')
+INSTANCE_ID=$(curl -H 'X-aws-ec2-metadata-token: $TOKEN' http://169.254.169.254/latest/meta-data/instance-id/)
+
+aws ec2 create-tags --region {aws_region} --resources $INSTANCE_ID --tags Key=Name,Value=\"${{GITHUB_REPOSITORY}}/${{GITHUB_RUN_ID}}/${{GITHUB_RUN_NUMBER}}/${{GITHUB_RUN_ATTEMPT}}/${{GITHUB_JOB}}\""));
+
     BASE64_STANDARD.encode(format!("{user_data_script}
+
+base64 -d '{tag_script}' > /home/runner/tag.sh
+chown runner:runner /home/runner/tag.sh
+
 ansible-pull --url https://github.com/drakon64/github-actions-runner-aws.git --checkout canary --extra-vars 'url=https://github.com/{repository_full_name}' --extra-vars 'token={repository_registration_token}' --extra-vars '{{ \"spot\": {spot} }}' --extra-vars 'ebs_volume_size={volume_size}' ansible/runner.yml"))
 }
