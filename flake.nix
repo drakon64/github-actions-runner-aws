e7412c595c429645fad05cbe4d4e110c7e752cfa
Adam Chance <6444703+drakon64@users.noreply.github.com>
2024-08-13 15:30:27 +0100

    Build with Crane instead of Naersk

--- flake.nix
+++ flake.nix
@@ -1,24 +1,38 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    naersk.url = "github:nix-community/naersk";
-    flake-utils.url = "github:numtide/flake-utils";
+
+    crane = {
+      url = "github:ipetkov/crane";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    flake-utils = {
+      url = "github:numtide/flake-utils";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs =
     {
       self,
       nixpkgs,
-      naersk,
+      crane,
       flake-utils,
+      ...
     }:
     flake-utils.lib.eachDefaultSystem (
       system:
       let
-        pkgs = (import nixpkgs) { inherit system; };
-        naersk' = pkgs.callPackage naersk { };
-      in {
-        defaultPackage = naersk'.buildPackage { src = ./.; };
+        pkgs = import nixpkgs { inherit system; };
+
+        craneLib = (crane.mkLib nixpkgs.legacyPackages.${system});
+        src = ./.;
+
+        cargoArtifacts = craneLib.buildDepsOnly { inherit src; };
+      in
+      {
+        defaultPackage = craneLib.buildPackage { inherit cargoArtifacts src; };
       }
     );
 }
